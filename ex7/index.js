232b32969507f3ad84ead9e56064412cdf315d41
PedroPapaCloudbeds <pedro.papa@cloudbeds.com>
2024-09-19 16:06:20 -0300

    fix if and else / fix magic number to a const / fix applesLess to a const with toFixed

--- ex7/index.js
+++ ex7/index.js
@@ -4,15 +4,16 @@ console.log("Insert the number of apples purchased:");
 const apples = Number(prompt(">"));
 
 function shop() {
+    const DEFAULT_PRICE = 0.3
+    const DISCOUNT_PRICE = 0.25
+
     if (apples < 12) {
-        let applesLess = (apples * 0.30);
-        applesLess = applesLess.toFixed(2);
-        return (`Your purchase will cost $${applesLess} dollars.`);
+        const finalPrice = (apples * DEFAULT_PRICE).toFixed(2);
+        return (`Your purchase will cost $${finalPrice} dollars.`);
     } 
-    if (apples >= 12) {
-        let applesMore = (apples * 0.25);
-        applesMore = applesMore.toFixed(2);
-        return (`Congrats!! You have a discount, your purchase will cost $${applesMore} dollars.`);
+    else {
+        const discountFinalPrice = (apples * DISCOUNT_PRICE).toFixed(2);
+        return (`Congrats!! You have a discount, your purchase will cost $${discountFinalPrice} dollars.`);
     }
 }
 
