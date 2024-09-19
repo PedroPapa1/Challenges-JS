ca94f900e410243bcf1b43befd47fce887027bc8
PedroPapaCloudbeds <pedro.papa@cloudbeds.com>
2024-09-19 15:57:03 -0300

    fix string prompt to number / fix function name / fix if and else

--- ex6/index.js
+++ ex6/index.js
@@ -1,36 +1,33 @@
 const prompt = require("prompt-sync")({ sigint: true});
 
 console.log("Insert side A: ");
-const sideA = prompt(">");
+const sideA = Number(prompt(">"));
 
 console.log("Insert side B: ");
-const sideB = prompt(">");
+const sideB = Number(prompt(">"));
 
 console.log("Insert side C: ");
-const sideC = prompt(">");
+const sideC = Number(prompt(">"));
 
- function verify() {
+ function checkIfItIsATriangle() {
     const isSideAValid = sideA < sideB + sideC;
     const isSideBValid = sideB < sideA + sideC;
     const isSideCValid = sideC < sideA + sideB;
 
-    if (isSideAValid && isSideBValid && isSideCValid) {
-        return true;
-    } 
-        return false;     
+    return isSideAValid && isSideBValid && isSideCValid  
 }
 
 function typeOfTriangle(){
     if (sideA === sideB && sideB === sideC) {
-        console.log("It's a Equilateral triangle.");
-    }else if (sideA === sideB || sideA === sideC || sideB === sideC) {
-        console.log("It's a Isosceles triangle.");
-    }else if (sideA !== sideB || sideB !==sideC) {
-        console.log("It's a Scalene triangle.");
+        console.log("It's an Equilateral triangle.");
+    } else if (sideA === sideB || sideA === sideC || sideB === sideC) {
+        console.log("It's an Isosceles triangle.");
+    } else {
+        console.log("It's an Scalene triangle.");
     }
 }
 
-if(verify()){
+if(checkIfItIsATriangle()){
     typeOfTriangle();
 }else {
     console.log("It's not a triangle.");
