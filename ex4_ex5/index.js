e7f15fb92a9344f520b143baa82a025d3d1fd807
PedroPapaCloudbeds <pedro.papa@cloudbeds.com>
2024-09-19 15:50:49 -0300

    Fix the magic number to a const

--- ex4_ex5/index.js
+++ ex4_ex5/index.js
@@ -7,14 +7,15 @@ console.log("Insert yout second grade: ");
 const secondGrade = prompt(">");
 
 
-function result() {
+function gradeResult() {
     let sum = (Number(firstGrade) + Number(secondGrade)) / 2;
+    const PASSING_GRADE = 6;
 
-    if (sum >= 6.0) {
+    if (sum >= PASSING_GRADE) {
         console.log(`Your grade is ${sum} congrats you have been approved!!`);
     } else {
         console.log(`Your grade is ${sum} don't give up!`);
     }
 }
 
-console.log(result());
+console.log(gradeResult());
