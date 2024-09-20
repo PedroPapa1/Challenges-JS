42de63f7f5b928434e89e4c2b1d0c3d88c0f7572
PedroPapaCloudbeds <pedro.papa@cloudbeds.com>
2024-09-20 15:00:27 -0300

    fix typo / fix the code to the exercise requirements

--- ex3/index.js
+++ ex3/index.js
@@ -2,23 +2,23 @@ const prompt = require("prompt-sync")({ sigint: true});
 
 const integerNumbers = [];
 for (let i = 0; i < 4; i++) {
-    console.log("Insert a integer number");
+    console.log("Insert an integer number");
     const input = prompt(">");
-    integerNumbers.push(Number(input));
+    integerNumbers.push(parseInt(input));
 }
 
 function calculate() {
-    integerNumbers[0] = integerNumbers[0] + 25;
-    console.log(integerNumbers[0]);
 
-    integerNumbers[1] = integerNumbers[1] * 3;
-    console.log(integerNumbers[1]);
-
-    integerNumbers[2] = (integerNumbers[2] * 12) / 100;
-    console.log(integerNumbers[2]);
-    
     integerNumbers[3] = integerNumbers[0] + integerNumbers[1] + integerNumbers[2]
+    integerNumbers[0] = integerNumbers[0] + 25;
+    integerNumbers[1] = integerNumbers[1] * 3;
+    integerNumbers[2] = (integerNumbers[2] * 12) / 100;
+
+    console.log(integerNumbers[0]);
+    console.log(integerNumbers[1]);
+    console.log(integerNumbers[2]);
     console.log(integerNumbers[3]);
+
 }
 
-console.log(calculate());
+calculate();
