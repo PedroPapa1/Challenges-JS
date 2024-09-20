ba8b10268598d62089520e69948659af6bf60df1
PedroPapaCloudbeds <pedro.papa@cloudbeds.com>
2024-09-20 15:06:05 -0300

    fix constant place / add a const for number 12

--- ex7/index.js
+++ ex7/index.js
@@ -3,11 +3,12 @@ const prompt = require("prompt-sync")({ sigint: true});
 console.log("Insert the number of apples purchased:");
 const apples = Number(prompt(">"));
 
-function shop() {
-    const DEFAULT_PRICE = 0.3
-    const DISCOUNT_PRICE = 0.25
+const DEFAULT_PRICE = 0.30;
+const DISCOUNT_PRICE = 0.25;
+const DISCOUNT_QUANTITY = 12;
 
-    if (apples < 12) {
+function shop() {
+    if (apples < DISCOUNT_QUANTITY) {
         const finalPrice = (apples * DEFAULT_PRICE).toFixed(2);
         return (`Your purchase will cost $${finalPrice} dollars.`);
     } 
