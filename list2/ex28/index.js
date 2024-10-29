7101ce1d9b290f04e84b2f9276588a20c2d86a14
PedroPapaCloudbeds <pedro.papa@cloudbeds.com>
2024-10-29 14:51:12 -0300

    feat: challenges part 2 ex28

---
+++ list2/ex28/index.js
@@ -0,0 +1,30 @@
+import { generateMatrix, printMatrix } from "../helper.js";
+
+export function SumOfElements() {
+  const rowCount = 4;
+  const columnCount = 4;
+  let sumAbove = 0;
+  let sumBelow = 0;
+
+  const matrix = generateMatrix({
+    rowCount,
+    columnCount,
+  });
+
+  printMatrix(matrix);
+  console.log("------------------");
+
+  for (let rowIndex = 0; rowIndex < rowCount; rowIndex++) {
+    for (let columnIndex = 0; columnIndex < columnCount; columnIndex++) {
+      if (rowIndex < columnIndex) {
+        sumAbove += matrix[rowIndex][columnIndex];
+      } else if (rowIndex > columnIndex) {
+        sumBelow += matrix[rowIndex][columnIndex];
+      }
+    }
+  }
+
+  console.log(`The sum of the elements above the main diagona: ${sumAbove}`);
+  console.log(`The sum of the elements below the main diagona: ${sumBelow}`);
+}
+SumOfElements();
