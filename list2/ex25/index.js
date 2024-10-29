fcf604a0fbe0524c67148a48333c08338f1a1a0b
PedroPapaCloudbeds <pedro.papa@cloudbeds.com>
2024-10-29 12:01:46 -0300

    feat: challenges part 2 ex25

---
+++ list2/ex25/index.js
@@ -0,0 +1,28 @@
+import { generateMatrix, printMatrix } from "../helper.js";
+
+export function columnSumOfMatrix() {
+  const rowCount = 15;
+  const columnCount = 20;
+  const columnCountArray = [];
+
+  const matrix = generateMatrix({
+    rowCount,
+    columnCount,
+  });
+
+  printMatrix(matrix);
+
+  for (let i = 0; i < columnCount; i++) {
+    columnCountArray[i] = 0;
+  }
+
+  for (let columnIndex = 0; columnIndex < columnCount; columnIndex++) {
+    for (let rowIndex = 0; rowIndex < rowCount; rowIndex++) {
+      columnCountArray[columnIndex] += matrix[rowIndex][columnIndex];
+    }
+  }
+
+  console.log("----------------------------");
+  console.log(JSON.stringify(columnCountArray));
+}
+columnSumOfMatrix();
