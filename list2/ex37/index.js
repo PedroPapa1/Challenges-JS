a7c2403221f699107adbafd2441fa1e69160f794
PedroPapaCloudbeds <pedro.papa@cloudbeds.com>
2024-11-06 14:27:40 -0300

    feat: challenges part 2 ex37

---
+++ list2/ex37/index.js
@@ -0,0 +1,40 @@
+import promptSync from "prompt-sync";
+const prompt = promptSync();
+
+const QUESTION_NUMBERS = 20;
+const STUDENT_NUMBERS = 50;
+const MEAN = 12;
+
+function examAnswers() {
+  const answerKey = [];
+  for (let i = 0; i < QUESTION_NUMBERS; i++) {
+    answerKey.push(Math.floor(Math.random() * 5) + 1);
+  }
+  return answerKey;
+}
+
+export function examScoreAnalysis() {
+  const answerKey = examAnswers();
+
+  for (let studentIndex = 0; studentIndex < STUDENT_NUMBERS; studentIndex++) {
+    const studentAnswerKey = [];
+    console.log(`Student (${studentIndex + 1}). Insert your answers, use just numbers (1-5).`);
+
+    for (let questionIndex = 0; questionIndex < QUESTION_NUMBERS; questionIndex++) {
+      console.log(`Question number ${questionIndex + 1}.`);
+      const studentAnswer = parseInt(prompt(">"));
+      studentAnswerKey.push(studentAnswer);
+    }
+    const correctQuestions = answerKey.filter((value) => studentAnswerKey.includes(value));
+
+    console.log(`That's the answer key: ${answerKey}`);
+    console.log(`These're your rigth answers: ${correctQuestions}`);
+
+    if (correctQuestions.length >= MEAN) {
+      console.log("APPROVED");
+    } else {
+      console.log("FAILED");
+    }
+  }
+}
+examScoreAnalysis();
