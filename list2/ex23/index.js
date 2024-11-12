af5570da8ee022d9f5b5de72e7da5a47700cc048
PedroPapaCloudbeds <pedro.papa@cloudbeds.com>
2024-11-12 12:21:48 -0300

    fix: remove function

--- list2/ex23/index.js
+++ list2/ex23/index.js
@@ -1,25 +1,23 @@
 const DIAGONAL_NUMBER = 1;
 const FILLING_NUMBER = 0;
+const ROW_BY_COLUMN = 7;
 
 export function identityMatrix() {
-    function calculate(rowByColumn) {
-      const matrix = [];
+  const matrix = [];
 
-      for (let column = 0; column < rowByColumn; column++) {
-        const arrayOfRow = [];
-        for (let arrayPosition = 0; arrayPosition < rowByColumn; arrayPosition++) {
-          if (column === arrayPosition) {
-            arrayOfRow.push(DIAGONAL_NUMBER);
-          } else {
-            arrayOfRow.push(FILLING_NUMBER);
-          }
-        }
-        matrix.push(arrayOfRow);
+  for (let column = 0; column < ROW_BY_COLUMN; column++) {
+    const arrayOfRow = [];
+    for (let arrayPosition = 0; arrayPosition < ROW_BY_COLUMN; arrayPosition++) {
+      if (column === arrayPosition) {
+        arrayOfRow.push(DIAGONAL_NUMBER);
+      } else {
+        arrayOfRow.push(FILLING_NUMBER);
       }
-      matrix.forEach(row => { 
-        console.log(JSON.stringify(row));
-      });  
+    }
+    matrix.push(arrayOfRow);
   }
-  calculate(7)
+  matrix.forEach((row) => {
+    console.log(JSON.stringify(row));
+  });
 }
 identityMatrix();
