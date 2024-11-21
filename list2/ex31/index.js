d0b0bac7913ef26479a275a2154b6b65e871beb6
PedroPapaCloudbeds <pedro.papa@cloudbeds.com>
2024-11-21 14:39:11 -0300

    fix: change function name / duplicating matrix the right way

--- list2/ex31/index.js
+++ list2/ex31/index.js
@@ -3,9 +3,9 @@ import promptSync from "prompt-sync";
 
 const prompt = promptSync();
 
-function comparingNumber({ matrix, columnCount, rowCount, comparativeNumber }) {
+function amountOfEqualNumbers({ matrix, columnCount, rowCount, comparativeNumber }) {
   let equalComaparativeNumber = 0;
-  const matrixFiltered = matrix;
+  const matrixFiltered = [...matrix];
 
   for (let columnIndex = 0; columnIndex < columnCount; columnIndex++) {
     for (let rowIndex = 0; rowIndex < rowCount; rowIndex++) {
@@ -33,16 +33,14 @@ export function elementCountAndMatrixFiltering() {
   console.log("Insert an INTEGER number.");
   const comparativeNumber = parseInt(prompt(">"));
 
-  const { equalComaparativeNumber, matrixFiltered } = comparingNumber({
+  const { equalComaparativeNumber, matrixFiltered } = amountOfEqualNumbers({
     matrix,
     columnCount,
     rowCount,
     comparativeNumber,
   });
 
-  console.log(
-    `There are ${equalComaparativeNumber} numbers equal to ${comparativeNumber} in the matrix.`
-  );
+  console.log(`There are ${equalComaparativeNumber} numbers equal to ${comparativeNumber} in the matrix.`);
   console.log(`The same matrix but without the number ${comparativeNumber}:`);
   printMatrix(matrixFiltered);
 }
